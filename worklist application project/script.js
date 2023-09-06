0e9f13ff3af57d5335df0105bf4e276bc4faa8c2
Prem1155 <123807687+Prem1155@users.noreply.github.com>
2023-09-06 22:23:10 +0530

    Add files via upload

---
+++ worklist application project/script.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const categorySelect = document.getElementById('categorySelect');
+  const workInput = document.getElementById('workInput');
+  const addButton = document.getElementById('addButton');
+  const changeTitleButton = document.getElementById('changeTitleButton');
+  const deleteButton = document.getElementById('deleteButton');
+  const clearButton = document.getElementById('clearButton');
+
+  addButton.addEventListener('click', addWork);
+  changeTitleButton.addEventListener('click', changeCardTitle);
+  deleteButton.addEventListener('click', deleteWork);
+  clearButton.addEventListener('click', clearWorklist);
+   
+  function addWork(){
+    const selectedCategory = categorySelect.value;
+    const workItem = document.createElement('li');
+    workItem.textContent = workInput.value;
+    document.getElementById(selectedCategory).appendChild(workItem);
+    workInput.value = '';
+  }
+  
+  function changeCardTitle() {
+    const selectedCategory = categorySelect.value;
+    const newTitle = prompt('Enter the new title:');
+    if (newTitle) {
+      const card = document.querySelector(`#${selectedCategory}`).parentNode;
+      card.querySelector('h2').textContent = newTitle;
+    }
+  }
+
+  function deleteWork() {
+    const selectedCategory = categorySelect.value;
+    const selectedCategoryElement = document.getElementById(selectedCategory);
+    const workItems = selectedCategoryElement.children;
+    if (workItems.length > 0) {
+      const confirmation = confirm('Are you sure you want to delete this work?');
+      if (confirmation) {
+        workItems[workItems.length - 1].remove();
+      }
+    }
+  }
+
+  function clearWorklist() {
+    const confirmation = confirm('Are you sure you want to clear the entire worklist?');
+    if (confirmation) {
+      const workItems = document.querySelectorAll(`#${categorySelect.value} span`);
+      workItems.forEach(function(workItem) {
+        workItem.remove();
+      });
+    }
+  }
+});
